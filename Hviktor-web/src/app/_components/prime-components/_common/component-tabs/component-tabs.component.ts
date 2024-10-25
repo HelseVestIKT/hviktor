8b22e00743a17ec77597f469a85b41c1745fc1dd
Erlend Johnsen <erea@ihelse.net>
2024-10-25 15:12:45 +0200

    Created reusable component for the code tab

---
+++ Hviktor-web/src/app/_components/prime-components/_common/component-tabs/component-tabs.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { TabsModule } from 'primeng/tabs';
+
+@Component({
+  selector: 'app-component-tabs',
+  standalone: true,
+  imports: [TabsModule, CardModule, DividerModule],
+  templateUrl: './component-tabs.component.html',
+  styleUrl: './component-tabs.component.scss'
+})
+export class ComponentTabsComponent {
+  @Input() import: string | undefined;
+}
--- Hviktor-web/src/app/_components/prime-components/tabs/tabs.component.ts
+++ Hviktor-web/src/app/_components/prime-components/tabs/tabs.component.ts
@@ -3,11 +3,12 @@ import { CardModule } from 'primeng/card';
 import { DividerModule } from 'primeng/divider';
 import { TabsModule } from 'primeng/tabs';  
 import { ComponentHeaderComponent } from '../_common/component-header/component-header.component';
+import { ComponentTabsComponent } from '../_common/component-tabs/component-tabs.component';
 
 @Component({
   selector: 'app-tabs',
   standalone: true,
-  imports: [TabsModule, DividerModule, CardModule, ComponentHeaderComponent],
+  imports: [TabsModule, DividerModule, CardModule, ComponentHeaderComponent, ComponentTabsComponent],
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss']
 })
