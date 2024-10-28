2c2a1b38c29b52fee9905af6a9db7bca4cfd0017
Erlend Johnsen <erea@ihelse.net>
2024-10-28 09:33:10 +0100

    colours component standalone

--- Hviktor-web/src/app/_components/colours/colours.component.ts
+++ Hviktor-web/src/app/_components/colours/colours.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-colours',
+  standalone: true,
+  imports: [],
   templateUrl: './colours.component.html',
   styleUrls: ['./colours.component.scss']
 })
--- Hviktor-web/src/app/app.module.ts
+++ Hviktor-web/src/app/app.module.ts
@@ -47,7 +47,6 @@ import { ComingSoonComponent } from './_components/hviktor-components/coming-soo
 @NgModule({
   declarations: [
     AppComponent,
-    ColoursComponent,
     TypographyComponent,
     HeaderComponent,
     DropdownComponent,
