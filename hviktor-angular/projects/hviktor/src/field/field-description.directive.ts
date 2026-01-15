dad93e19b876cf76499f7ce9ef909b48e4afdd65
Erlend Johnsen <erea@ihelse.net>
2026-01-15 09:34:02 +0100

    adds field description

---
+++ hviktor-angular/projects/hviktor/src/field/field-description.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from "@angular/core";
+
+@Directive({
+    selector: '[hviFieldDescription]',
+    standalone: true,
+    host: {
+        '[attr.data-field]': 'description',
+        id: 'field-description',
+    },
+})
+export class HviFieldDescriptionDirective {}
