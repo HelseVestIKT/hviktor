78f901a69d0d41322438f228f8d3034cb9abd6b1
SusannLundekvam <susann.lundekvam@helse-vest-ikt.no>
2026-02-19 17:01:52 +0100

    test5

--- hviktor-icons/projects/icons/src/lib/base-icon.component.ts
+++ hviktor-icons/projects/icons/src/lib/base-icon.component.ts
@@ -1,31 +1,34 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, computed } from '@angular/core';
 
 @Component({
   selector: 'hvi-icon-base',
   imports: [],
-  standalone: true,
   template: `
     <svg
-      [attr.width]="sizePx"
-      [attr.height]="sizePx"
+      [attr.width]="sizePx()"
+      [attr.height]="sizePx()"
       viewBox="0 0 24 24"
       fill="currentColor"
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path [attr.d]="pathData"></path>
+      <path [attr.d]="pathData()"></path>
     </svg>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  styles: ``,
+  styles: `
+    :host {
+      display: inline-block;
+      line-height: 0;
+    }
+  `,
 })
 export class HviIconBase {
-  @Input() size: 'sm' | 'md' | 'lg' = 'md';
-  @Input() pathData: string = '';
+  size = input<'sm' | 'md' | 'lg'>('md');
+  pathData = input.required<string>();
 
-    get sizePx() {
-    return this.size === 'sm' ? 16 :
-           this.size === 'lg' ? 32 : 24;
-  }
-
+  sizePx = computed(() => {
+    return this.size() === 'sm' ? 16 :
+           this.size() === 'lg' ? 32 : 24;
+  });
 }
