a35e27bc539586e5db7fa77feff32d00e0a586de
SusannLundekvam <susann.lundekvam@helse-vest-ikt.no>
2026-02-19 17:16:14 +0100

    test6

--- hviktor-icons/projects/icons/src/lib/icons/icon-home.component.ts
+++ hviktor-icons/projects/icons/src/lib/icons/icon-home.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { HviIconBase } from '../base-icon.component';
-
+import { ChangeDetectionStrategy, Component, input, computed } from '@angular/core';
 
 @Component({
   selector: 'hvi-icon-home',
-  standalone: true,
-  imports: [HviIconBase],
   template: `
-    <hvi-icon-base pathData="M12 3l9 9h-3v9h-12v-9h-3z"></hvi-icon-base>
+    <svg
+      [attr.width]="sizePx()"
+      [attr.height]="sizePx()"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M12 3l9 9h-3v9h-12v-9h-3z"></path>
+    </svg>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: `
+    :host {
+      display: inline-block;
+      line-height: 0;
+    }
   `
 })
+export class HviIconHome {
+  size = input<'sm' | 'md' | 'lg'>('md');
 
-export class HviIconHome {}
+  sizePx = computed(() => {
+    return this.size() === 'sm' ? 16 :
+           this.size() === 'lg' ? 32 : 24;
+  });
+}
--- hviktor-icons/projects/icons/src/lib/icons/icon-user.component.ts
+++ hviktor-icons/projects/icons/src/lib/icons/icon-user.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { HviIconBase } from '../base-icon.component';
-
+import { ChangeDetectionStrategy, Component, input, computed } from '@angular/core';
 
 @Component({
   selector: 'hvi-icon-user',
-  standalone: true,
-  imports: [HviIconBase],
   template: `
-    <hvi-icon-base pathData="M12 3l9 9h-3v9h-12v-9h-3z"></hvi-icon-base>
+    <svg
+      [attr.width]="sizePx()"
+      [attr.height]="sizePx()"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
+    </svg>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: `
+    :host {
+      display: inline-block;
+      line-height: 0;
+    }
   `
 })
+export class HviIconUser {
+  size = input<'sm' | 'md' | 'lg'>('md');
 
-export class HviIconUser {}
+  sizePx = computed(() => {
+    return this.size() === 'sm' ? 16 :
+           this.size() === 'lg' ? 32 : 24;
+  });
+}
