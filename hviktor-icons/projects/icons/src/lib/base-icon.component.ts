c67b34abf38adce1a934da8b83dc4eef3e61abba
SusannLundekvam <susann.lundekvam@helse-vest-ikt.no>
2026-02-26 12:50:25 +0100

    samla komponent i base-icon

--- hviktor-icons/projects/icons/src/lib/base-icon.component.ts
+++ hviktor-icons/projects/icons/src/lib/base-icon.component.ts
@@ -1,34 +1,37 @@
 import { ChangeDetectionStrategy, Component, input, computed } from '@angular/core';
 
 @Component({
-  selector: 'hvi-icon-base',
-  imports: [],
   template: `
-    <svg
+   <svg
       [attr.width]="sizePx()"
       [attr.height]="sizePx()"
       viewBox="0 0 24 24"
-      fill="currentColor"
+      fill="none"
+      [attr.stroke]="color()"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path [attr.d]="pathData()"></path>
-    </svg>
-  `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+      <path [attr.d]="path" />
+    </svg>`,
   styles: `
     :host {
       display: inline-block;
       line-height: 0;
     }
   `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HviIconBase {
+export abstract class HviIconBase {
   size = input<'sm' | 'md' | 'lg'>('md');
-  pathData = input.required<string>();
+  color = input<string>('currentColor');
 
-  sizePx = computed(() => {
-    return this.size() === 'sm' ? 16 :
-           this.size() === 'lg' ? 32 : 24;
+  protected readonly sizePx = computed(() => {
+    const sizeMap = { sm: 16, md: 24, lg: 32 };
+    return sizeMap[this.size()];
   });
+
+  protected abstract readonly path: string;
 }
