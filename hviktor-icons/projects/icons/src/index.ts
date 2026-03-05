72c035441c32267049901b4f39631738420001ee
SusannLundekvam <susann.lundekvam@helse-vest-ikt.no>
2026-03-05 12:25:42 +0100

    delete icons project

--- hviktor-icons/projects/icons/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { registerIconsAsCustomElements } from './lib/register-elements';
-
-bootstrapApplication(class {}).then(() => {
-  registerIconsAsCustomElements();
-});
--- hviktor-icons/projects/icons/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of icons
- */
-
-export * from './lib/icons/icon-user.component';
-export * from './lib/icons/icon-home.component';
-
