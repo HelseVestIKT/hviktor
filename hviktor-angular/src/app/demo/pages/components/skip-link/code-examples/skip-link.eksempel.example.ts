c7db5632194efa9348c41f03d2d5ac03437408de
Erlend Johnsen <erea@ihelse.net>
2026-03-05 13:57:31 +0100

    fixes dark mode error on skiplink

--- hviktor-angular/src/app/demo/pages/components/skip-link/code-examples/skip-link.eksempel.example.ts
+++ hviktor-angular/src/app/demo/pages/components/skip-link/code-examples/skip-link.eksempel.example.ts
@@ -7,21 +7,16 @@ import { HviParagraph, HviSkipLink } from '@helsevestikt/hviktor';
   imports: [HviParagraph, HviSkipLink],
   template: `
     <div
-      class="relative grid gap-2 rounded border border-dashed border-neutral-300 p-4 focus-within:ring-2 focus-within:ring-blue-500"
+      class="relative grid gap-2 rounded border border-dashed p-4 focus-within:ring-2"
       tabindex="0"
     >
       <p hviParagraph class="text-center">
-        Klikk her og trykk <kbd class="rounded border bg-neutral-100 px-1 py-0.5">Tab</kbd> for å se
-        SkipLink
+        Klikk her og trykk <kbd class="rounded border px-1 py-0.5">Tab</kbd> for å se SkipLink
       </p>
       <a hviSkipLink href="#demo-main-content">Hopp til hovedinnhold</a>
     </div>
 
-    <main
-      id="demo-main-content"
-      tabindex="-1"
-      class="mt-4 rounded border border-neutral-200 bg-neutral-50 p-4 focus:outline-2 focus:outline-blue-500"
-    >
+    <main id="demo-main-content" tabindex="-1" class="mt-4 rounded border p-4 focus-within:ring-2">
       <p hviParagraph>
         <strong>Hovedinnhold:</strong> Dette er regionen som mottar fokus fra SkipLink.
       </p>
