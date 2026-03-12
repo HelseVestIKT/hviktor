ed57b0cb29c8f1fbcb0810500791188d4904ba56
SusannLundekvam <susann.lundekvam@helse-vest-ikt.no>
2026-03-12 14:38:25 +0100

    update

--- hviktor-angular/projects/icons/src/index.new.ts
+++
@@ -1,8 +0,0 @@
-// Main entry point - imports no icons by default for tree-shaking
-// Import individual icons as needed:
-// import '@helsevestikt/hviktor-icons/dist/lib/webcomponents/icon-airplane.webcomponent';
-// Or import all at once (larger bundle):
-// import '@helsevestikt/hviktor-icons/all';
-
-// Export base class if consumers want to extend it
-export { HviIconBase } from './lib/base-icon.webcomponent';
