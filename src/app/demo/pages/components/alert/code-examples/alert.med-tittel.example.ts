87320695a256be60554a9bb532557d84ddc50bb4
Erlend Johnsen <erea@ihelse.net>
2026-05-07 12:13:08 +0200

    feat(alert): add examples for alert component with and without title

---
+++ src/app/demo/pages/components/alert/code-examples/alert.med-tittel.example.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { HviAlert, HviParagraph } from '@helsevestikt/hviktor';
+
+@Component({
+  selector: 'app-alert-med-tittel-example',
+  standalone: true,
+  imports: [HviAlert, HviParagraph],
+  template: `
+    <div class="flex flex-wrap gap-2">
+      <hvi-alert title="Har du husket å bestille passtime?">
+        <p hviParagraph>
+          Det er lange køer for å bestille pass om dagen, det kan være lurt å bestille i god tid før
+          du skal reise.
+        </p>
+      </hvi-alert>
+    </div>
+  `,
+})
+export class AlertMedTittelExampleComponent {}
