f22067f22e559fc995fae7009f6defdd17828c44
Erlend Johnsen <erea@ihelse.net>
2026-06-11 09:45:47 +0200

    test(fieldset): add JSDoc, unit tests, and E2E tests

--- projects/hviktor/src/forms/fieldset/fieldset.directive.ts
+++ projects/hviktor/src/forms/fieldset/fieldset.directive.ts
@@ -1,30 +1,37 @@
 import { Directive } from '@angular/core';
 
 /**
- * Fieldset is used to group and label fields that naturally belong together, such as date fields or address fields.
- * The component helps organize information, make forms more manageable, and improve accessibility for screen readers.
+ * @summary Directive that applies Designsystemet fieldset styling to a native `<fieldset>` element,
+ * grouping related form fields with a shared legend for improved structure and accessibility.
  *
- * @example
+ * @example Radio group fieldset
  * ```html
- *  <fieldset hviFieldset>
- *    <legend hviLabel>Which framework do you like best?</legend>
- *    <p hviParagraph>Your choice will help us improve the service.</p>
- *    <hvi-field>
- *      <input type="radio" id="angular" name="framework" value="angular" />
- *      <label hviLabel for="angular">Angular</label>
- *    </hvi-field>
- *    <hvi-field>
- *      <input type="radio" id="react" name="framework" value="react" />
- *      <label hviLabel for="react">React</label>
- *    </hvi-field>
- *    <hvi-field>
- *      <input type="radio" id="vue" name="framework" value="vue" />
- *      <label hviLabel for="vue">Vue</label>
- *    </hvi-field>
- *  </fieldset>
+ * <fieldset hviFieldset>
+ *   <legend hviLabel weight="medium">Which framework do you like best?</legend>
+ *   <p hviParagraph>Your choice will help us improve the service.</p>
+ *   <hvi-field>
+ *     <input hviInput type="radio" id="angular" name="framework" value="angular" />
+ *     <label hviLabel for="angular">Angular</label>
+ *   </hvi-field>
+ *   <hvi-field>
+ *     <input hviInput type="radio" id="react" name="framework" value="react" />
+ *     <label hviLabel for="react">React</label>
+ *   </hvi-field>
+ * </fieldset>
  * ```
  *
- * Documentation: https://designsystemet.no/en/components/docs/fieldset/overview
+ * @example Disabled fieldset
+ * ```html
+ * <fieldset hviFieldset disabled>
+ *   <legend hviLabel weight="medium">Disabled group</legend>
+ *   <hvi-field>
+ *     <input hviInput type="checkbox" id="opt" value="opt" />
+ *     <label hviLabel for="opt">Option</label>
+ *   </hvi-field>
+ * </fieldset>
+ * ```
+ *
+ * @see {@link https://designsystemet.no/komponenter/fieldset}
  */
 @Directive({
   selector: 'fieldset[hviFieldset]',
